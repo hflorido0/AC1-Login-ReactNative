ebbe77d965f5e5603667ea8d792a39115e1fadd5
yitan <yitan57@hotmail.com>
2024-02-01 22:24:26 +0100

    update

---
+++ src/view/TableShow.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { View } from 'react-native';
+
+export default function TableShow() {
+
+    const[usuarios, setUsuarios] = useState([]);
+
+    return (
+        <View>TEST</View>
+    )
+}
